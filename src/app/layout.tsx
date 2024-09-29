6f4128546ab966e516379f18f13c0c88dc0db2bb
Erick Watanabe <erickjwatanabe@gmail.com>
2024-09-29 13:52:13 -0700

    Style: add background colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,17 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={`$(inter.className) bg-slate-50 text-gray-900`}>
+				<div
+					className="bg-slate-100 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[32.25rem] rounded-full blur-[10rem] md:w-[68.75rem]
+				"
+				></div>
+				<div
+					className="bg-stone-200 absolute top-[-1rem] -z-10 left-[-35rem] w-[50rem] rounded-full blur-[10rem] md:w-[68.75rem] md:left-[33rem] lg:left-[-28rem]
+				"
+				></div>
+				{children}
+			</body>
 		</html>
 	);
 }
