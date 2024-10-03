a630e04434ce38a97aad7725d321578958df3d9b
Erick Watanabe <erickjwatanabe@gmail.com>
2024-10-03 15:28:33 -0700

    Populate experience data

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,10 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
+import DealerSysLandingPageImg from "@/public/DealerSysLandingPageImg.png";
+import AnimosHomePageImg from "@/public/AnimosHomePageImg.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
+import ValterTennisImg from "@/public/ValterTennis.png";
 
 export const links = [
 	{
@@ -35,52 +36,44 @@ export const links = [
 
 export const experiencesData = [
 	{
-		title: "Graduated bootcamp",
-		location: "Miami, FL",
+		title: "Graduated Hack Reactor Bootcamp",
+		location: "Los Angeles, CA",
 		description:
-			"I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+			"After 4 months of intense study I have the tools to be a full stack developer",
 		icon: React.createElement(LuGraduationCap),
-		date: "2019",
+		date: "2023",
 	},
 	{
-		title: "Front-End Developer",
-		location: "Orlando, FL",
+		title: "Full-Stack Engineer Contractor",
+		location: "Long Beach, CA",
 		description:
-			"I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-		icon: React.createElement(CgWorkAlt),
-		date: "2019 - 2021",
-	},
-	{
-		title: "Full-Stack Developer",
-		location: "Houston, TX",
-		description:
-			"I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+			"My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
 		icon: React.createElement(FaReact),
-		date: "2021 - present",
+		date: "2024 - present",
 	},
 ] as const;
 
 export const projectsData = [
 	{
-		title: "CorpComment",
+		title: "Valter Pavia Tennis Academy",
 		description:
 			"I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
 		tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-		imageUrl: corpcommentImg,
+		imageUrl: ValterTennisImg,
 	},
 	{
-		title: "rmtDev",
+		title: "Animos",
 		description:
-			"Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-		tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-		imageUrl: rmtdevImg,
+			"Single-page application allowing users to create and attend pet-friendly events. I was the full-stack developer. It has features like creating and listing pet friendly events.",
+		tags: ["React", "FastAPI", "JavaScript", "Python 3", "Redux", "MongoDB"],
+		imageUrl: AnimosHomePageImg,
 	},
 	{
-		title: "Word Analytics",
+		title: "DealerSys",
 		description:
-			"A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-		tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-		imageUrl: wordanalyticsImg,
+			"A single-page application that keeps track of a car dealership’s inventory, sales, and services.",
+		tags: ["React", "Django 4", "PostgreSQL", "Python 3"],
+		imageUrl: DealerSysLandingPageImg,
 	},
 ] as const;
 
