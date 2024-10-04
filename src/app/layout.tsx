a1caa262f82d22254beb8f30c59cde80f2c53afa
Erick Watanabe <erickjwatanabe@gmail.com>
2024-10-04 11:53:46 -0700

    Refactor style: Changed classNames to use flexbox

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,15 +17,18 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={`$(inter.className) bg-slate-50 text-gray-900`}>
+			<body
+				className={`${inter.className} bg-slate-50 text-gray-900 relative h-[5000px]`}
+			>
 				<div
-					className="bg-slate-100 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[32.25rem] rounded-full blur-[10rem] md:w-[68.75rem]
+					className="bg-slate-300 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[32.25rem] rounded-full blur-[10rem] md:w-[68.75rem]
 				"
 				></div>
 				<div
-					className="bg-stone-200 absolute top-[-1rem] -z-10 left-[-35rem] w-[50rem] rounded-full blur-[10rem] md:w-[68.75rem] md:left-[33rem] lg:left-[-28rem]
+					className="bg-stone-400 absolute top-[-1rem] -z-10 left-[-35rem] w-[50rem] rounded-full blur-[10rem] md:w-[68.75rem] md:left-[33rem] lg:left-[-28rem]
 				"
 				></div>
+				<Header />
 				{children}
 			</body>
 		</html>
