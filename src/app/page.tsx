97159c49c69d7cee30967bbdca040658ce7c7c91
Erick Watanabe <erickjwatanabe@gmail.com>
2024-10-10 11:02:22 -0700

    Add section divider after intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/Intro";
+import SectionDivider from "@/components/Section-divider";
 
 export default function Home() {
 	return (
-		<main className="flex justify-center">
+		<main className="flex flex-col justify-center items-center">
 			<Intro />
+			<SectionDivider />
 		</main>
 	);
 }
