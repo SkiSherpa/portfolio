d8cf3c8fa90222359b20c5e58035881e7d6909b3
Erick Watanabe <erickjwatanabe@gmail.com>
2024-10-10 14:58:28 -0700

    Fix styling

--- src/components/About-me.tsx
+++ src/components/About-me.tsx
@@ -16,9 +16,24 @@ export default function AboutMe() {
 			id="about"
 		>
 			<h2 className="text-2xl font-bold sm:text-3xl capitalize">About me</h2>
-			<p>
-				I'm a Physics Professor turned Software Engineer. I have three years of
-				programming experience.
+			<p className="mb-3">
+				A <span className="font-medium">Physics Professor</span> turned{" "}
+				<span className="font-medium">Software Engineer</span>. After 12 years
+				of teaching I wanted a change. I enrolled in a coding bootcamp and
+				learned <span className="font-medium">full-stack web development</span>.{" "}
+				<span className="italic">My favorite part of programming</span> is the
+				flow building applications and the problem-solving aspect. I{" "}
+				<span className="underline">love</span> falling into the the{" "}
+				<span className="font-medium">flow</span> of coding and revel in solving
+				problems that come with creating applications. My core stack is{" "}
+				<span className="font-medium">
+					React, Next.js, JavaScript, Python 3
+				</span>
+				, and <span className="font-medium">MongoDB</span>. I am also familiar
+				with TypeScript, Tailwind, PostgreSQL, and Firebase. I am always looking
+				to learn new technologies. I am currently looking for a{" "}
+				<span className="font-medium">full-time position</span> as a software
+				Engineer.
 			</p>
 		</motion.section>
 	);
