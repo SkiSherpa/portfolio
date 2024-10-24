4a686cb8802592c6f7fe27d15ba7e0be6b2134d9
Erick Watanabe <erickjwatanabe@gmail.com>
2024-10-24 14:43:08 -0700

    change border color for layout and styling fixing

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -13,16 +13,16 @@ export default function ContactMe() {
 			className="mb-20 sm:mb-28 w-[min(100%,38rem)]"
 		>
 			<SectionHeading>Contact me</SectionHeading>
-			<p>
+			<p className="text-gray-700">
 				Please contact me directly at{" "}
 				<a className="underline" href="erickjwatanabe@gmail.com">
 					erickjwatanabe@gmail.com
 				</a>{" "}
 				or through this form.
 			</p>
-			<form>
-				<input type="email"></input>
-				<textarea />
+			<form className="mt-10">
+				<input className="h-14 rounded-lg borderBlack" type="email"></input>
+				<textarea className="h-52 my-3 rounded-lg p-4 borderBlack" />
 				<button type="submit">
 					Submit <FaPaperPlane />
 				</button>
