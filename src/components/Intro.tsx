8abb620717bec11ee0acc40633b8079439e142bb
Erick Watanabe <erickjwatanabe@gmail.com>
2024-10-25 11:01:41 -0700

    Fix header highlight smoothness for contact me button in Intro component

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -8,9 +8,11 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { SiGitlab } from "react-icons/si";
 import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
 	const { ref } = useSectionInView("Home", 0.25);
+	const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
 	return (
 		<section
@@ -55,6 +57,10 @@ export default function Intro() {
 				<Link
 					href="#contact"
 					className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full border-2 focus:scale-110 hover:scale-110 hover:bg-sky-600 active:scale-105 transition"
+					onClick={() => {
+						setActiveSection("Contact");
+						setTimeOfLastClick(Date.now());
+					}}
 				>
 					Contact me here
 					<BsArrowRight className="opacticy-70 group:hover:translate-x-1 transition" />
