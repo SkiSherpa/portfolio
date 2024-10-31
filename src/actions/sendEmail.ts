b8d889b58d71d39983d5f63bc66b4f7089cd189f
Erick Watanabe <erickjwatanabe@gmail.com>
2024-10-31 16:00:07 -0700

    Add Error handling on server side

--- src/actions/sendEmail.ts
+++ src/actions/sendEmail.ts
@@ -11,6 +11,21 @@ const validateString = (value: unknown, maxLength: number) => {
 	return true;
 };
 
+const getErrorMessage = (error: unknown): string => {
+	let message: string;
+
+	if (error instanceof Error) {
+		message = error.message;
+	} else if (error && typeof error === "object" && "message" in error) {
+		message = String(error.message);
+	} else if (typeof error === "string") {
+		message = error;
+	} else {
+		message = "Something went wrong";
+	}
+	return message;
+};
+
 export const sendEmail = async (formData: FormData) => {
 	const message = formData.get("message") || "invalid message";
 	const senderEmail = formData.get("senderEmail");
@@ -27,11 +42,17 @@ export const sendEmail = async (formData: FormData) => {
 		};
 	}
 
-	resend.emails.send({
-		from: `onboarding@resend.dev`,
-		to: ["erickjwatanabe@gmail.com"],
-		subject: "MessAGE from portfolio page",
-		text: message as string,
-		replyTo: senderEmail as string,
-	});
+	try {
+		resend.emails.send({
+			from: `onboarding@resend.dev`,
+			to: ["erickjwatanabe@gmail.com"],
+			subject: "MessAGE from portfolio page",
+			text: message as string,
+			replyTo: senderEmail as string,
+		});
+	} catch (error: unknown) {
+		return {
+			error: getErrorMessage(error),
+		};
+	}
 };
