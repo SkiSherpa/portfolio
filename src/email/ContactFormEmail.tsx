ad08f3b77cc3dc6cceafeda4b7273dcfcef28e5d
Erick Watanabe <erickjwatanabe@gmail.com>
2024-11-01 12:51:57 -0700

    Add styling to email sent

--- src/email/ContactFormEmail.tsx
+++ src/email/ContactFormEmail.tsx
@@ -27,10 +27,10 @@ export default function ContactFormEmail({
 			<Head />
 			<Preview>New message from your portfolio site</Preview>
 			<Tailwind>
-				<Body>
+				<Body className="bg-gray-200">
 					<Container>
-						<Section>
-							<Heading>
+						<Section className="bg-white borderBlack my-10 px-10 rounded-md">
+							<Heading className="leading-tight">
 								You recieved the following message from your contact formd
 							</Heading>
 							<Text>{message}</Text>
