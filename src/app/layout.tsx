c04d71e13cdfbcb07052454ad0d0dcd05322e4d8
Erick Watanabe <erickjwatanabe@gmail.com>
2024-11-04 14:45:50 -0800

    Change styling of background and text colors for light and dark mode. Change colors for theme button to standout against background in light and dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,16 +22,10 @@ export default function RootLayout({
 	return (
 		<html lang="en" className="!scroll-smooth">
 			<body
-				className={`${inter.className} bg-slate-50 text-gray-900 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-100`}
+				className={`${inter.className} bg-stone-50 text-gray-900 relative pt-28 sm:pt-36 dark:bg-zinc-800 dark:text-zinc-200`}
 			>
-				<div
-					className="bg-slate-300 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[32.25rem] rounded-full blur-[10rem] md:w-[68.75rem]
-				"
-				></div>
-				<div
-					className="bg-stone-400 absolute top-[-1rem] -z-10 left-[-35rem] w-[50rem] rounded-full blur-[10rem] md:w-[68.75rem] md:left-[33rem] lg:left-[-28rem]
-				"
-				></div>
+				<div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+				<div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 				<ActiveSectionContextProvider>
 					<Header />
 					{children}
