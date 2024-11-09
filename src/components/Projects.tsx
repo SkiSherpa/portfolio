092781309b6f2cdfdabdd715070455a918db4f51
Erick Watanabe <erickjwatanabe@gmail.com>
2024-11-09 13:24:09 -0800

    Add magrin to projects in mobile

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,7 +10,11 @@ export default function Projects() {
 	const { ref } = useSectionInView("Projects", 0.75);
 
 	return (
-		<section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+		<section
+			ref={ref}
+			id="projects"
+			className="scroll-mt-28 mb-28 ml-2 mr-2 sm:ml-0 sm:mr-0"
+		>
 			<SectionHeading>My projects</SectionHeading>
 			<div>
 				{projectsData.map((project, index) => (
