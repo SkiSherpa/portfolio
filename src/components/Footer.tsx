6c5ceacc3e72e0b8da54648f1231acc78ac7d247
Erick Watanabe <erickjwatanabe@gmail.com>
2024-11-11 14:28:06 -0800

    Fix typo in foooter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,9 @@ export default function Footer() {
 				&copy; 2024 Erick Watanabe. All rights reserved.
 			</small>
 			<p className="text-sm">
-				<span className="semi-bold">About this webiste:</span> built with React&
-				Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer
-				Motion, React Email & Resend, Vercel hosting.
+				<span className="semi-bold">About this webiste:</span> built with React
+				& Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
+				Framer Motion, SendGrid, Vercel hosting.
 			</p>
 		</footer>
 	);
