694b85aac2004d6f9da031e5cc2a1e1b2ad37409
Erick Watanabe <erickjwatanabe@gmail.com>
2026-01-28 11:08:20 -0800

    Add visible border to theme swtich button

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -8,7 +8,7 @@ export default function ThemeSwitch() {
 
 	return (
 		<button
-			className="fixed bottom-5 right-5 bg-orange-50 w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] borderBlack border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-900 dark:text-zinc-200 z-10"
+			className="fixed bottom-5 right-5 bg-orange-50 w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border-opacity-40 shadow-2xl rounded-full flex items-center justify-center border-sky-600 border-2 hover:scale-[1.15] active:scale-105 transition-all dark:border-sky-600 dark:bg-gray-900 dark:text-zinc-200 z-10"
 			onClick={() => setTheme(theme)}
 		>
 			{theme === "light" ? <BsSun /> : <BsMoon />}
